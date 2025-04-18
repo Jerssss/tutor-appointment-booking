dea20c40e4f54c74dd0ab4b12fe11c30f2f8b767
Gab <alysongabrielflores@gmail.com>
2025-04-18 12:44:36 +0800

    updated some admin fxml and classes

--- src/main/java/client/admin/view/AdminCreateTutorView.java
+++ src/main/java/client/admin/view/AdminCreateTutorView.java
@@ -1,4 +1,50 @@
 package client.admin.view;
 
+import javafx.animation.ScaleTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.util.Duration;
+
 public class AdminCreateTutorView {
+
+    @FXML
+    private Button refreshButton;
+    @FXML
+    private Button addTutorButton;
+
+    public void addTutorButtonExited() {
+        ScaleTransition st = new ScaleTransition(Duration.millis(200), addTutorButton);
+        st.setToX(1.0);
+        st.setToY(1.0);
+        st.setCycleCount(1);
+        st.setAutoReverse(false);
+        st.play();
+    }
+
+    public void addTutorButtonHovered() {
+        ScaleTransition st = new ScaleTransition(Duration.millis(200), addTutorButton);
+        st.setToX(0.9);
+        st.setToY(0.9);
+        st.setCycleCount(1);
+        st.setAutoReverse(false);
+        st.play();
+    }
+
+    public void refreshButtonExited() {
+        ScaleTransition st = new ScaleTransition(Duration.millis(200), refreshButton);
+        st.setToX(1.0);
+        st.setToY(1.0);
+        st.setCycleCount(1);
+        st.setAutoReverse(false);
+        st.play();
+    }
+
+    public void refreshButtonHovered() {
+        ScaleTransition st = new ScaleTransition(Duration.millis(200), refreshButton);
+        st.setToX(0.9);
+        st.setToY(0.9);
+        st.setCycleCount(1);
+        st.setAutoReverse(false);
+        st.play();
+    }
 }
